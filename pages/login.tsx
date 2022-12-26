d6fde5724375400298dfecee6b7c8878cf71f2a0
ilfanrismawan <irsthirteen@gmail.com>
2022-12-27 06:33:44 +0700

    success

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,43 @@
+import Head from "next/head"
+import Image from "next/image"
+import { useState } from "react"
+
 function Login() {
+    const [login, setLogin] = useState(false)
+
     return (
-        <div>
-            loginbro
+        <div className="relative flex h-scren w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+            <Head>
+                <title>Netflix - Where to Watch?</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Image
+                src="https://rb.gy/p2hphi"
+                alt="Background Image Login Screen"
+                fill
+                className="-z-10 !hidden opacity-60 sm:inline"
+                style={{ objectFit: "cover" }}
+
+            />
+            <img src="https://rb.gy/ulxxee" alt="logo" className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6" width={150} height={150} />
+
+            <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+                <h1 className="text-4xl font-semibold">Sign In</h1>
+                <div className="space-y-4">
+                    <label className="inline-block w-full">
+                        <input type="email" placeholder="Email" className="input" />
+                    </label>
+
+                    <label className="inline-block w-full">
+                        <input type="password" placeholder="Password" className="input" />
+                    </label>
+                </div>
+                <button className="w-full rounded bg-[#e50914] py-3 font-semibold">Sign In</button>
+                <div className="text-[gray]">
+                    New to WheretoWatch?{' '}
+                    <button type="submit" className="text-white hover:underline">Sign up now</button>
+                </div>
+            </form>
         </div>
     )
 }
